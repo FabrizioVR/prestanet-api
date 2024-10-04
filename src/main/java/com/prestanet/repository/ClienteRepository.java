2375adbe8d7f2f99cd0593c274cb9b4c4a53e3e7
fab <fabrix336@gmail.com>
2024-10-04 18:50:49 -0500

    CRUD de cliente funcionando

--- src/main/java/com/prestanet/repository/ClienteRepository.java
+++ src/main/java/com/prestanet/repository/ClienteRepository.java
@@ -1,4 +1,7 @@
 package com.prestanet.repository;
 
-public interface ClienteRepository {
+import com.prestanet.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 }
--- src/main/java/com/prestanet/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/prestanet/service/impl/ClienteServiceImpl.java
@@ -1,4 +1,51 @@
 package com.prestanet.service.impl;
 
-public class ClienteServiceImpl {
+
+import com.prestanet.model.entity.Cliente;
+import com.prestanet.repository.ClienteRepository;
+import com.prestanet.service.ClienteService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente findById(int id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        return cliente.orElse(null); // Devuelve null si no se encuentra
+    }
+
+    @Override
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public Cliente update(int id, Cliente cliente) {
+        if (!clienteRepository.existsById(id)) {
+            return null; // Devuelve null si el cliente no existe
+        }
+        cliente.setIdCliente(id); // Asegúrate de que el id se mantenga
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void delete(int id) {
+        clienteRepository.deleteById(id);
+    }
 }
