7b8f7626b28692f50c3b3a15fd3a2112bfda2ddf
fab <fabrix336@gmail.com>
2024-10-04 11:47:41 -0500

    First commit

---
+++ src/main/java/com/prestanet/PrestanetApiApplication.java
@@ -0,0 +1,13 @@
+package com.prestanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestanetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrestanetApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prestanet/PrestanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestanetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
